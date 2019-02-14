ee70b8cadc90450c554f5d86a21081d6ef646dd0
Luis Lopez <delucholuis@gmail.com>
2019-02-14 09:39:45 -0800

    ip addres

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '3.91.214.8',
+      host: '54.197.20.222',
       username: 'ubuntu',
       privateKey: 'travis-key.pem'
     })
